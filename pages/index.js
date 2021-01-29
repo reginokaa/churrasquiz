682814b66b53fcca658d76035b6f43557523be92
Regina <rfonseca87@hotmail.com>
2021-01-29 15:49:32 -0300

    finalizando a  imersão react

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import {motion} from 'framer-motion';
+import {useRouter} from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
@@ -10,6 +12,7 @@ import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import Link from '../src/components/Link';
 
 /*
 function Title(props) { //propcidades/propriedades
@@ -48,11 +51,22 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
+      <title>
         AluraQuiz - ChurrasquiZ
+      </title>
       </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{delay: 0, duration: 0.5}}
+          variants={{
+            show: {opacity: 1, y: '0'},
+            hidden: {opacity: 0, y: '100%'},
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>
               {db.title}
@@ -83,11 +97,42 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{delay: 0.5, duration: 0.5}}
+          variants={{
+            show: {opacity: 1},
+            hidden: {opacity: 0},
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1> Quizzes da Galera </h1>
 
-            <p> 🚧 logo menos 🚧 </p>
+            {/* <p> 🚧 logo menos 🚧 </p> */}
+
+            <ul>
+            {db.external.map((linkExterno) => {
+                const [projectName, userName] = linkExterno
+                .replace(/\//g, '')
+                .replace('https:', '')
+                .replace('.vercel.app', '')
+                .split('.');
+
+              return (
+                <li key={linkExterno}>
+                <Widget.Topic
+                  as={Link}
+                  href={`/quiz/${projectName}___${userName}`}
+                >
+                  {`${userName}/${projectName}`}
+                </Widget.Topic>
+              </li>
+            );
+          })}
+        </ul>
+
           </Widget.Content>
         </Widget>
         <Footer />
--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz'
 
-export default function QuizDaGaleraPage() {
+export default function QuizDaGaleraPage({dbExterno}) {
+
   return (
-    <div>
-      Desafio da próxima aula junto com as animações
-    </div>
+
+    <ThemeProvider theme={dbExterno.theme}>
+
+      <QuizScreen 
+        externalQuestions={dbExterno.questions} 
+        externalBg={dbExterno.bg}
+      />
+
+    </ThemeProvider>
+
+      // <pre style={{color: 'black'}}>
+      //   {JSON.stringify(dbExterno.questions, null, 4)}
+      // </pre>
+   
   );
 }
+
+export async function getServerSideProps(context) {
+
+  const [projectName, userName] = context.query.id.split('___');
+  
+
+  try {
+    const dbExterno = await fetch(`https://${projectName}.${userName}.vercel.app/api/db`)
+    
+      .then((respostaDoServer) => {
+        
+        if (respostaDoServer.ok) {
+
+          return respostaDoServer.json();
+
+        }
+        throw new Error('Falha em pegar os dados');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+
+      return {
+        props: {
+          dbExterno,
+        },
+      };
+    } catch(err) {
+      throw new Error(err);
+      
+    }
+    console.log(err)
+  }
+// sempre que receber uma chamada direto nessa url via servidor, essa página vai carregar um código que vai rodar no lado do backend
+// viabilizando colocar algum redirecionamento a partir do servidor 
